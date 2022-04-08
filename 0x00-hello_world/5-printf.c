b7f06d1fad1a1327c99aaf3556d82ef43ca8ec44
Adama Dolo <doloadama@outlook.com>
2022-04-08 06:29:09 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,14 @@
 *
 * Return always 0 (Success)
 */
+
+/**
+ *the main function 
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+/** print a text and go to a new line
+*/
