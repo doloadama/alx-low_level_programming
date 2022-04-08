31e590e69c8b270d0030e630125ae235e31e9992
Adama Dolo <doloadama@outlook.com>
2022-04-08 12:34:23 +0000

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
- printf("is positive");
+printf("is positive\n");
 else
- print("is negative");
+printf("is negative\n");
 }
 return (0);
 }
