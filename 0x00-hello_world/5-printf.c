9ba4da4d20f5c2cc1a751f0ba6e3c47db4d8b6d7
Adama Dolo <doloadama@outlook.com>
2022-04-08 06:23:17 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- *
- * Return always 0 (Success)
- */
+* Main - Entry point
+*
+* Return always 0 (Success)
+*/
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}  
+} 
