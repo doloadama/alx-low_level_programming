e09564372545156dad62b416ee90a0248c487155
Adama Dolo <doloadama@outlook.com>
2022-04-08 20:45:40 +0000

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  char alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 putchar(tolower(alphabet)\n);
 return (0);
 }
