eb73d5a409057f84157823d72abb9b53f466853a
Adama Dolo <doloadama@outlook.com>
2022-04-08 19:15:18 +0000

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,12 +6,13 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int last_digit = 0;
+last_digit = 0;
 last_digit = n % 10;
 if (last_digit < 6 && last_digit != 0)
 printf("Last digit of %d is lessthan 6 and not 0", n);
