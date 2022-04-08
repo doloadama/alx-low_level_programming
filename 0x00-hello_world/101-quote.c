222f30ca4ef7ff8c241c06caa6a370c7c8bf52bb
Adama Dolo <doloadama@outlook.com>
2022-04-08 09:54:44 +0000

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char a = 'and that piece of art is useful"'
+char b = ' - Dora Korpar, 2015-10-19\n'
+return (a)
+return (1)
+}
