7804bf963645871c350a14f2697312e565d3baf3
Adama Dolo <doloadama@outlook.com>
2022-04-08 20:42:09 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+putchar(tolower(alphabet)\n);
+return (0);
+}
