194f8c2fa905f09b79cbab93ff70e466c13bcf97
doloadama <doloadama@outlook.com>
2022-04-09 20:27:32 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + C);
+putchar('\n');
+return (0);
+}
