9c93f8ae8c48e54069d6440df08535fee0f8a992
doloadama <doloadama@outlook.com>
2022-04-10 12:46:35 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+for (c = '0'; c <= 9; ++c)
+putchar(c);
+putchar(',');
+putchar(' ';
+putchar('\n');
+return (0);
+}
