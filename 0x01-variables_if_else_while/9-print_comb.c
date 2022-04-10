d8916e582d210fd9161186fc5d9f96011bcd5682
doloadama <doloadama@outlook.com>
2022-04-10 12:50:41 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-char c;
-for (c = '0'; c <= 9; ++c)
-putchar(c);
+int c;
+for (c = 0; c <= 9; ++c)
+putchar('0' + c);
 putchar(',');
- putchar(' ');
+putchar(' ');
 putchar('\n');
 return (0);
 }
