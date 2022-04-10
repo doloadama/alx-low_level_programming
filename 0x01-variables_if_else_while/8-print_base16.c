6a2f29156a647b4e091f847227adef56a96826c5
doloadama <doloadama@outlook.com>
2022-04-10 12:27:59 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+for ( c = 0; c <= 15; ++c)
+hexa = c / 16;
+putchar('0' + hexa);
+putchar ('\n');  
+return (0);
+}
