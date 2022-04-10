3da80406d54c8505b98e64c9652a3f316ea09d95
doloadama <doloadama@outlook.com>
2022-04-10 11:23:05 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+for (c = 0; c < 9; c++)
+printf(c);
+printf("\n");
+return (0);
+}
