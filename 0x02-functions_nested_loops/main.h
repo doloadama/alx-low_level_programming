0bc1ad38dc9935e72ed633e14403afe961066aba
doloadama <doloadama@outlook.com>
2022-04-12 22:08:02 +0000

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,17 @@
 #define _MAIN_H_
 
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+return (1);
+}
+
+
+
+
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isaplha(int c);
