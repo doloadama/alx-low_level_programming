ee3712b84911800d5fab03aa5326207301808bb9
doloadama <doloadama@outlook.com>
2022-04-13 05:34:57 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - return the alphabet 10 times
+ */
+
+
+void print_alphabet_x10(void)
+{
+char c;
+int n;
+while(n < 9)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
+_putchar(c);
+}
+_putchar('\n');
+n++;
+}
