aa3c74dff167bb2027ed20129547d97418e748dd
doloadama <doloadama@outlook.com>
2022-04-13 08:56:22 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - check the value of an integer
+ * print the absolute value of that integer
+ */
+
+int _abs(int)
+{
+int n;
+int result;
+ 
+if (n < 0)
+{
+result = n * (-1);
+putchar(result);
+else
+putchar(n);
+}
+}
