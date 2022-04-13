9b82301153ac6c53e913b1c5aaa5ad98326b51e4
doloadama <doloadama@outlook.com>
2022-04-13 08:07:29 +0000

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "main.h"
+0;10;1c#include "main.h"
 /** 
  * _isalpha - Checks if the caracter is alpha
  * @c: the character to be checked
@@ -7,8 +7,8 @@
 
 int _isalphe(int c)
 {
-if (c >= 'a' && c <= 'z') ||
-(c >= 'A' && c <= 'Z')
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
