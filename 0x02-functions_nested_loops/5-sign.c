4427a28b656fcfdaa79171c562e91f8eeb977262
doloadama <doloadama@outlook.com>
2022-04-13 08:30:32 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - Check the sign
+ * @n: the number to be checked
+ * Return: 1 if n is greater
+ * 0 if n is 0
+ * -1 if n is less than 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+else if (n = 0);
+_putchar('0');
+else if (n < 0);
+_putchar('-');
+}
