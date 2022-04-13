a0d93a3d7aae6b3dcff5c539e1b18cee5bc9dba1
doloadama <doloadama@outlook.com>
2022-04-13 20:32:59 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - Prints all natural numbers
- * @n: the parameter 
+ * @n: the parameter
  */
 
 void print_to_98(int n)
@@ -11,12 +11,12 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-printf("%d, ", n--);  
+printf("%d, ", n--);
 printf("%d\n", n);
 }
 else
 {
-while(n < 98)
+while (n < 98)
 printf("%d, ", n++);
 printf("%d\n", n);
 }
