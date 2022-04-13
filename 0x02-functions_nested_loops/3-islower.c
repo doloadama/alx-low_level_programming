9c08d6ca680f1756b9cb10d7b8afe251fc961307
doloadama <doloadama@outlook.com>
2022-04-13 06:38:47 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,8 @@
 /**
  * _islower - check if a caracter is lowercase
  * @c: the character to be checked
- * Return : 1 if charachter is lowercase
+ * Return: 1 if charachter is lowercase
+ * Return: 0 if character is uppercase
  */
 
 int _islower(int c)
