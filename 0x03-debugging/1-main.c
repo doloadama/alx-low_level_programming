610ffb2cb308f5282d23eb3c5d03583083ce1037
doloadama <doloadama@outlook.com>
2022-04-14 13:07:32 +0000

    task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,7 +15,7 @@ i = 0;
 
 while (i < 10)
 {
-putchar(i); /* @i: will remain 0 if not incremented  */
+putchar(i); /* the variable i will not be incremented */
 }
 
 printf("Infinite loop avoided! \\o/\n");
