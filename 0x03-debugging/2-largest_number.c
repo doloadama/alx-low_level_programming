71d064745961bbebc415322ea0a27372ccccb216
doloadama <doloadama@outlook.com>
2022-04-14 20:17:34 +0000

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,18 @@
 
 int largest_number(int a, int b, int c)
 {
-  int largest;
-  if (a > b && b > c)
-    {
-      largest = a;
-    }
-     else if (b > a && a > c)
-       {
-	largest = b;
-       }
-     else if ( c > a ; a > b)
+int largest;
+if (a >= b && b >= c)
+{
+largest = a;
+}
+if (b >= a && a >= c)
+{
+largest = b;
+}
+if ( c >= a ; a >= b)
+{
+largest = c;
+}
+return (largest);
+}
