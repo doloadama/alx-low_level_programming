b66fe19d363f9a6e7423304f32aa26a8e58b6289
doloadama <doloadama@outlook.com>
2022-04-15 23:20:49 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++);
+for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
 {
@@ -19,7 +19,7 @@ else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-else if (i % 5 == 0);
+else if (i % 5 == 0)
 {
 if (i != 100)
 {
