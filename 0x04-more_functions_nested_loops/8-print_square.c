70fb7532a5752fa6fd9096f9a0804648da95938a
doloadama <doloadama@outlook.com>
2022-04-15 22:25:19 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: the number of line
+ */
+
+void print_square(int size)
+{
+int i, j;
+if (n > 0)
+{
+for i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
