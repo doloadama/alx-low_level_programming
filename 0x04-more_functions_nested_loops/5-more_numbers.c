915e3d7781cf679afa0b028a0ece76f6eb572830
doloadama <doloadama@outlook.com>
2022-04-15 20:37:13 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,12 @@ while (n <= 9)
 {
 for (c = 0 ; c <= 14 ; c++)
 {
-_putchar('0' + c);
+if (c > 9)
+{
+_putchar('1');
 }
-n++;
+_putchar((c % 10) + '0');
+}
 _putchar('\n');
 }
-_putchar('\n');
 }
