df29d8ebe382019c886a9b5b76ee038a3800c148
doloadama <doloadama@outlook.com>
2022-04-15 15:14:17 +0000

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isdigit - check for a digit
- * @c: the parameter to be checked
- * Return: 1 if c is digit or 0 otherwise
+ * _isdigit - check for a digit.
+ * @c: is digit.
+ * Return: 1 if c is digit or 0 otherwise.
  */
 
 int _isdigit(int c)
