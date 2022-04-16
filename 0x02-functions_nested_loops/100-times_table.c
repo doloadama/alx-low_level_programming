6bb04a9ac3fd77c21d7a605a2099de201c129d0c
doloadama <doloadama@outlook.com>
2022-04-16 15:34:12 +0000

    task 102

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,11 +4,9 @@
  * print_times_table - Print n times a table
  * @n: the integer
  */
-
 void print_times_table(int n)
 {
 int multi, num, prod;
-
 if (!(n > 15 || n < 0))
 {
 for (num = 0; num <= n; num++)
@@ -21,14 +19,12 @@ if (multi != 0)
 _putchar(',');
 _putchar(' ');
 }
-
 if (prod < 10 && multi != 0)
 {
 _putchar(' ');
 _putchar(' ');
 _putchar((prod % 10) + '0');
 }
-
 else if (prod < 100 && prod >= 10)
 {
 _putchar(' ');
@@ -41,7 +37,6 @@ _putchar((prod / 100) + '0');
 _putchar((prod / 10) % 10 + '0');
 _putchar((prod % 10) + '0');
 }
-  
 else
 _putchar((prod % 10) + '0');
 }
