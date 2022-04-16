0d70afb78d022d255af74c517e531dff17f82ff8
doloadama <doloadama@outlook.com>
2022-04-16 18:02:42 +0000

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,9 @@ int main(void)
 int n, res, tree = 0, five = 0;
 for (n = 0; n < 1024; n++)
 {
-if (n % 3 == 0)
+if (n % 3 == 0 || n % 5 == 5)
 {
-tree += n;
-}
-if (n % 5 == 0)
-{
-five += n;
-}
+res += n;
 }
 res = tree + five;
 printf("%d", res);
