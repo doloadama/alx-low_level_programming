d7f95264b3b2a070d094082096d6c184de92c9c6
doloadama <doloadama@outlook.com>
2022-04-16 18:13:16 +0000

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,7 +8,9 @@ int main(void)
 {
 int n, res;
 for (n = 0; n < 1024; n++)
-if (n % 3 == 0 || n % 5 == 5)
+if (n % 3 == 0 )
+res += n;
+else if(n % 5 == 5)
 res += n;
 printf("%d", res);
 printf("\n");
