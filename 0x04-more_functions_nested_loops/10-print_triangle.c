350d4740169a3c4c04ddb7e07098676f08a1823e
Dolo Adama <doloadama@outlook.com>
2022-04-18 22:51:51 +0000

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,21 +12,18 @@ void print_triangle(int size)
 {
 int row;
 int column;
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
 for (row = 0; row < size; row++)
 {
 for (column = 0; column > size - row; column++)
+{
 _putchar(' ');
 for (column = size - row - 1; column <= size; column++)
+{
 _putchar('#');
+if (column == size -1 && row != size -1)
 _putchar('\n');
 }
-
 }
-
+_putchar('\n');
+}
 }
