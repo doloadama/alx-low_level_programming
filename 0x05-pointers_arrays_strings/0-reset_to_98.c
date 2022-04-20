88a98aa3fc84a7486b4a8fd1d06efef72929616a
Dolo Adama <doloadama@outlook.com>
2022-04-20 22:37:23 +0000

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update a value 
+ * reset_to_98 - update a value
  * @n: the parameter
  */
 
 void reset_to_98(int *n)
 {
-int p;
-n = &p;
 *n = 98;
 
 }
