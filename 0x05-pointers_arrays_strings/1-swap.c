fd8a6957715bd308036f5207a53047dfb28af597
Dolo Adama <doloadama@outlook.com>
2022-04-20 22:41:52 +0000

    swap the values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap the value of two integers
+ * @a: a pointer and a parameter of the function
+ * @b: a pointer and a parameter of the function
+ */
+
+void swap_nt(int *a, int *b)
+{
+*a = b;
+*b = a;
+}
