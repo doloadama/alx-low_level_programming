eeac55b7cd980db1b460cc039a21962918f99c3c
Dolo Adama <doloadama@outlook.com>
2022-04-20 23:07:45 +0000

    swap the values

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the lenght of a string
+ * @s: a parameter and also a pointer
+ */
+
+int _strlen(char *s);
+{
+int i = 0;
+while (s[i] != '\0');
+i++;
+return (i);
+}
