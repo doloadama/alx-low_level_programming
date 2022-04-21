9f933410db63761ee8afdc033fd30eebe93ff64a
Dolo Adama <doloadama@outlook.com>
2022-04-21 22:02:47 +0000

    function that print every other character of a string, strarting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints evry other character of a string 
+ * puts2 - prints evry other character of a string
  * @str: the parameter
  */
 
@@ -14,11 +14,7 @@ if (str[0] != '\0')
 do {
 _putchar(str[i]);
 i += 2;
-}while ((str[i - 1] != '\0') && (str[i] != '\0'));
+} while ((str[i - 1] != '\0') && (str[i] != '\0'));
 }
- _putchar('\n');
-
-
-
-  
+_putchar('\n'); 
 }
