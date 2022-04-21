23b404167453b9eb10fc9a7275288aa9fda4e141
Dolo Adama <doloadama@outlook.com>
2022-04-21 22:50:37 +0000

    functions that prints half of a string

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: a pointer
+ * @n: an integer
+ */
+
+void print_array_int *a, int n)
+{
+int i = 0;
+for (i = 0; i < n; ++i)
+{
+if (i == n -1)
+printf("%d", *(a + n -1));
+else
+printf("%d", *(a + 1));
+}
+printf("\n");
+}
