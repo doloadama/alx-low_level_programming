b88da5cd416c7f6919af5047d64f44c4b609a52b
Dolo Adama <doloadama@outlook.com>
2022-04-21 05:45:27 +0000

    Reversed String

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,13 +16,13 @@ return (i);
 void print_rev(char *s)
 {
 int  leng;
- int i;
-int *spec = s;
+int i;
 
-leng = _strlen(s[]);
- for (i = leng -1; i >=0 ; i--)
-   {
-     _putchar(s[i]);
-   }
- _putchar('\n');
+
+leng = _strlen(s);
+for (i = leng -1; i >=0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
