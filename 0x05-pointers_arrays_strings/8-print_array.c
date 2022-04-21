b52aba8bfe3f53d5169a3c495d3b40c518458ee5
Dolo Adama <doloadama@outlook.com>
2022-04-21 23:01:36 +0000

    functions that prints half of a string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,10 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-for (i = 0; i < n; ++i)
+for (i = 0; i <= n - 1; ++i)
 {
 if (i == n - 1)
-printf("%d", *(a + n - 1));
+printf("%d", *(a + n));
 else
 printf("%d, ", *(a + 1));
 }
