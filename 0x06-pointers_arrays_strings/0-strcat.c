1fbde6dc950d98716c3b1fe15cbb6100fb17d863
Dolo Adama <doloadama@outlook.com>
2022-04-23 19:13:17 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,11 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 
-for (i = 0 ; dest[i] != '\0'; i++)
+while(dest[i++] != '\0')
 {
-for (j = 0; src[j] != '\0'; j++)
+while(src[j++] != '\0')
 {
 dest[i] = src [j];
-i++;
 }
 return (dest);
 }
