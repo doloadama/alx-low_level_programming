32fcb4739fd0a181db82a1e6644528eece06130c
Dolo Adama <doloadama@outlook.com>
2022-04-24 21:03:31 +0000

    functio that upper the letter in lowercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,14 @@
 
 /**
  * string_toupper - functions that changes lowercase to uppercase
+ * @str: string
+ * Return: char
  */
 
 char *string_toupper(char *str)
 {
 int i;
-
+i = 0;
 while(*(str + i))
 {
 if (*(str + i) >= 'a' && (*str + i) <= 'z')
