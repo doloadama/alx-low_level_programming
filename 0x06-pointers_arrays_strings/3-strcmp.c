b9783267d182dbdec1ad458d4b8f7b5272f1f4b8
Dolo Adama <doloadama@outlook.com>
2022-04-24 15:46:47 +0000

    function that compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,11 @@ int _strcmp(char *s1, char *s2)
 
 int *i = s1;
 int *j = s2;
-while (*i != '\0' && *j != '\0' && *i == *j)
+while (s1[i] != '\0' && s2[j] != '\0' && s1[i] == s2[j])
 {
 i++;
 j++;
 }
-test = (*i - *j);
+test = (s1[i] - s2[j]);
 return (test);
 }
