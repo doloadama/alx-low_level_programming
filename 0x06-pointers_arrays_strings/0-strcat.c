9ae326d82df0333038075f37dbbf810fef70880a
Dolo Adama <doloadama@outlook.com>
2022-04-24 10:12:10 +0000

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,9 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
-for (i = 0; dest[i] != '\0'; i++)
-{
-if (dest[i] == '\0')
-{
-for (j = 0; src[j] != '\0'; j++)
-{
+while(dest[i])
+i++;
+for (j = 0; src[j]; j++)
 dest[i] = src[i];
-}
-}
-}
-dest[i] = '\0';
 return (dest);
 }
