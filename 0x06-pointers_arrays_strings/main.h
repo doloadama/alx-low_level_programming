8be1d5c4a363c5ce7c61feec013c0a9b2d907927
Dolo Adama <doloadama@outlook.com>
2022-04-25 19:28:59 +0000

    Function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H_
 
 
-void _putchar();
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -12,4 +12,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif /*_MAIN_H_*/
