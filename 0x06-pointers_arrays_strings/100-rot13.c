416126078953780db74a3a4078426eef5a2db536
Dolo Adama <doloadama@outlook.com>
2022-04-25 05:59:28 +0000

    Function that encode a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @c: the string to be encoded
+ * Return : c
+ */
+
+
+char *rot13(char *c)
+{
+int i, j;
+char a[]="ABCDEFGHIJKLM";
+char b[]="NOPQRSTUVWXYZ";
+for (i = 0; *(c + i); i++)
+{
+for (j = 0; j < 13; j++)
+{
+if (a[j] == c[i])
+{
+c[i] = b[j];
+}
+}
+
+}
+return (c);
+}
