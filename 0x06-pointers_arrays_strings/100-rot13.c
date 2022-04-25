baf9c270cc32085f74a4e7ecb857de106ea94580
Dolo Adama <doloadama@outlook.com>
2022-04-25 08:21:04 +0000

    encode a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,16 +10,14 @@
 char *rot13(char *c)
 {
 int i, j;
-char a[] = "ABCDEFGHIJKLM";
-char b[] = "NOPQRSTUVWXYZ";
+char a[] = "abcdefghijklmABCDEFGHIJKLM";
+char b[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
 for (i = 0; *(c + i); i++)
 {
 for (j = 0; j <= 12; j++)
 {
 if (a[j] == c[i])
 c[i] = b[j];
-else
-c[i] = '\0';
 }
 }
 return (c);
