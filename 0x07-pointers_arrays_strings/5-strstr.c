c609fa2b16fbdc10994ee4cd9c4eda67356db2b5
Dolo Adama <doloadama@outlook.com>
2022-04-27 22:37:40 +0000

    Function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#define NULL 0
 /**
  * _strstr - locates a substring
  * @haystack: the string
@@ -24,7 +24,7 @@ if (needle[i] == needle[j]
 return (haystack + i);
 }
 }
-i ++;
+i++;
 }
 return(NULL)
 }
