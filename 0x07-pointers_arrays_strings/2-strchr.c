b6495831fe940ebeaea7249c76ed74036a752cdb
Dolo Adama <doloadama@outlook.com>
2022-04-27 21:50:51 +0000

    Function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,13 @@
  * Return: s
  */
 
-char *_strcher(char *s, char c)
+char *_strchr(char *s, char c)
 {
 int i = 0;
 while ( s[i] != '\0' && s[i] != c)
-{
 i++;
 if (s[i] == c)
 return(&s[i]);
 else
 return (NULL);
-}
 }
