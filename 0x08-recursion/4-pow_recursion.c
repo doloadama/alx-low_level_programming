af4b7b77d165af99886b489c4ce65c8a38ce1a05
Dolo Adama <doloadama@outlook.com>
2022-04-29 03:16:06 +0000

    Function that returns the the powered value of n

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _pow_recursion - returns the value of x raised to the power of y
- * @x: the value 
+ * @x: the value
  * @y: the power
  * Return: the value of x raised to the power of y
  */
