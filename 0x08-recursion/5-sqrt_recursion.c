c44c021c5965a3d23537c3bc7310b7c1f54a6b9c
Dolo Adama <doloadama@outlook.com>
2022-04-29 19:55:49 +0000

    Function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,26 @@
 int _sqrt_recursion(int n)
 {
 
-if ( n == 1)
-{
-return (1);
-}
-else if (n < 0)
+if ( n <= 0)
 {
 return (-1);
 }
-return (n * _sqrt_recursion(n + 1));
+return (my_sqrt_recursion(n, 0);
+}
+
+/**
+ *my_sqrt_recursion - THis is my sqrt recursion
+ * @a: the entry
+ * @n: the sum
+ * Return: the result
+ */
+
+int my_sqrt_recursion(int a, int b)
+{
+if (a == (b * b))
+return (b);
+else if ((b * b) >= a)
+return (-1);
+else
+return (my_sqrt_recursion(a, b + 1));
 }
