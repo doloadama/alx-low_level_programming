8cb817e97698a1cca2596f81eb4b37539e5db02e
Dolo Adama <doloadama@outlook.com>
2022-05-03 20:29:08 +0000

    Function that prints the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: the size of the command line
+ * @argv: a pointer of array that contain the arguments of commands line
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+print("%s", argv[0]);
+return (0);
+}
