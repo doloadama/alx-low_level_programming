d2c38b53e3a6b77efd0b4463297db3e1613bad3d
Dolo Adama <doloadama@outlook.com>
2022-05-03 22:35:42 +0000

    Function that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive number
+ * @argc : the arguments counter
+ * @argv: the arguments array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int sum = 0, i;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+int b;
+char *str;
+
+str = argv[i];
+for (b = 0; str[b] != '\0'; b++)
+{
+if (str[b] < 48 || str[b] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
