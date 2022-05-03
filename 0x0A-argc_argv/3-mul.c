dfd8752f651d3f633dea78153aa1d4dec0770f2f
Dolo Adama <doloadama@outlook.com>
2022-05-03 22:17:17 +0000

    Function that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Multiplies two numbers
+ * @argc: the counter
+ * @argv: the array
+ * Return:
+ */
+
+int main(int argc, char *argv[])
+{
+int i, multi;
+multi = 1;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+for (i = 1; i < argc; i++)
+{
+multi = multi * atoi(argv[i]);
+}
+printf("%d\n", multi);
+return (0);
+}
