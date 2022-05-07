6fb4cc36a84a4bd258c343bd6d6d1a9090ea7504
Dolo Adama <doloadama@outlook.com>
2022-05-07 17:42:23 +0000

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *s;
-s = malloc(size * sizeof(char));
+ s = malloc((size + 1) * sizeof(char));
 if (s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
