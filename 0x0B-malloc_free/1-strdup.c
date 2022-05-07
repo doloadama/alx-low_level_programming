9c248fbd03f1224ecfbf6836f512135933f73506
Dolo Adama <doloadama@outlook.com>
2022-05-07 15:27:15 +0000

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ int a, b;
 if (str  == NULL)
 return (NULL);
 for (a = 0; str[a] != '\0'; a++)
-c = (char *) malloc((a - 1) * sizeof(str));
+c = (char *) malloc((a) * sizeof(char));
 if (c == NULL)
 return (NULL);
 
