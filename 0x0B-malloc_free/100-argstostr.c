6293dbee80c1b21c871930d00645d2c2bf290115
Dolo Adama <doloadama@outlook.com>
2022-05-08 17:16:23 +0000

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,37 +12,70 @@
 
 char *argstostr(int ac, char **av)
 {
-int size, c, c1, c2 = 0;
-char *src;
+#include "holberton.h"
+
+/**
+ * _strcat - Concat string, src appended to dest
+ *
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Concat newString to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+for (a = 0; dest[a] != '\0'; a++)
+;
+for (b = 0; src[b] != '\0'; b++)
+{
+dest[a] = src[b];
+a++;
+}
+return (dest);
+}
+
+/**
+ * _strlen - Return the number of the length of the string
+ *
+ * @s: This is the string to check
+ *
+ * Return: Return the length of the string
+ */
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+;
+return (a);
+}
+
+/**
+ * argstostr - Concats argv to new string
+ *
+ * @ac: Argument count
+ * @av: Arguments
+ *
+ * Return: Returns a pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+int index, sumString;
+char *newString;
+sumString = 1;
 if (ac == 0 || av == NULL)
+return (0);
+for (index = 0; index < ac && av[index]; index++)
 {
+sumString += _strlen(av[index]);
+}
+newString = malloc((sumString * sizeof(char)) + ac);
+for (index = 0; index < ac && av[index]; index++)
+{
+newString = _strcat(newString, av[index]);
+newString = _strcat(newString, "\n");
+}
+if (newString == NULL)
 return (NULL);
-}
-for (c = 0; c < ac; c++)
-{
-for (c1 = 0; av[c][c1] != '\0'; c1++)
-{
-size += 1;
-}
-size += 1;
-src = malloc(sizeof(char) * size);
-}
-size += 1;
-if (src == NULL)
-{
-free(src);
-return (NULL);
-}
-for (c = 0; c < ac; c++)
-{
-for (c1 = 0; av[c][c1] != '\0'; c1++)
-{
-src[c2] = av[c][c1];
-c2++;
-}
-src[c2] = '\n';
-c2++;
-}
-src[c2] = '\0';
-return (src);
+return (newString);
 }
