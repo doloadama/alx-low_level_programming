ec22b61d0db49f9ea271ffdf3953ae8bdde5db66
Dolo Adama <doloadama@outlook.com>
2022-05-09 21:02:21 +0000

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - a struct with the following elements
+ * @name: the name member
+ * @age: the age member
+ * @owner: the owner member
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
