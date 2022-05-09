d6c462ae644e07b5bf396a80d6bd8f93a8b365e6
Dolo Adama <doloadama@outlook.com>
2022-05-09 23:00:18 +0000

    new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog -Dog data.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Desctiption: a structure with the name, age and owner of a dog.
+**/
 
 
 typedef struct dog 
@@ -9,11 +15,8 @@ char *name;
 float age;
 char *owner;
 }dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-
 #endif /* _DOG_H_ */
