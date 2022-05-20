7efacf91df3ce8984bb627b641511fa76087d8dd
Adama Dolo <doloadama@outlook.com>
2022-05-20 21:15:57 +0000

    A function that returns the number of elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,13 @@ size_t print_list(const list_t *h)
 unsigned int i;
 const list_t *a;
 a = h;
-list_t.str = malloc(sizeof(list_t));
 for (i = 0; a; i++)
 {
-if (str == NULL)
+if (a->str == NULL)
 {
 printf("[0] (nil)");
 }
+ 
 printf("[%u] %s\n", a->len, a->str);
 a = a->next;
 }
