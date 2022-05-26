09b4b128824cdbdab463df4b4b9d121a22d26278
Adama Dolo <doloadama@outlook.com>
2022-05-26 21:17:40 +0000

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 int diviseur, verif;
 if (index > (sizeof(unsigned long int) * 8 - 1))
-return (-1);   
+return (-1);
 
 diviseur = 1 << index;
 verif = n & diviseur;
