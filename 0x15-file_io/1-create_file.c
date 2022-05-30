a73ba52c76418cc3605d5768d3ef5037ee9a9252
Dolo Adama <doloadama@outlook.com>
2022-05-30 22:59:04 +0000

    function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int ln = 0;
 if (filename == NULL)
 return (-1);
 
- fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 if (fd == -1)
 return (-1);
