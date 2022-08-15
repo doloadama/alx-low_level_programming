dd5f5e617d5879080496f579c8baca55ad53da86
doloadama <doloadama@outlook.com>
2022-08-15 16:39:48 +0000

    A function to print the nulber of nodes of a DDL

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a double linked list.
+ * @h: The double linked lists to prints.
+ * Return; the number of node of the DDL.
+ */
+
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t n = 0;
+
+while (h != NULL)
+{
+printf("%i\n", h->n);
+h = h->next;
+n++;
+}
+return (n);
+}
