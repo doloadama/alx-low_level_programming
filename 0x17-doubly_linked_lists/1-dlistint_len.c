47744c845ddeadf9af8eb9d91e686bdb7c78898b
doloadama <doloadama@outlook.com>
2022-08-15 16:47:07 +0000

    A function that counts the number of elements in a DDL

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * dlistint_len - Counts the number of elements in a doubly linked list
+ * @h: the doubly linked list
+ * Return: the number of elements in the DDL
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+int longueur = 0;
+
+while (h != NULL)
+{
+++longueur;
+h = h->next;
+}
+
+return (longueur);
+}
