b68b67ed5633a9507499eb8e0acd88610dbda464
doloadama <doloadama@outlook.com>
2022-08-22 05:45:00 +0000

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,42 +1,30 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
-
 /**
-* hash_table_create - Creates a hash table.
-* @size: Size of the array.
-* Return: A pointer 2 the newly created hash table.
-*/
+ * hash_table_create - creates a hash table
+ * @size: size of the table
+ * Return: hash table on success, NULL on failure.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new_table;
-unsigned long int index;
-/*Handle errors*/
-if (size == 0)
-return (NULL);
 
+	hash_table_t *hash_table;
 
-/*Allocated memory*/
-new_table = malloc(sizeof(hash_table_t));
-/*Handle errors*/
-if (new_table == NULL)
-return (NULL);
+	if (size == 0)
+		return (NULL);
 
+	hash_table = malloc(sizeof(hash_table_t));
 
-/*Allocated memory*/
-new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table == NULL)
+		return (NULL);
 
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	return (hash_table);
 
-if (new_table->array == NULL)
-return (NULL);
-
-
-new_table->size = size;
-
-
-for (index = 0; index < size; index++)
-new_table->array[index] = NULL;
-
-
-return (new_table);
 }
