2552f55095d6a57bc470478978ce786982a4c5bd
doloadama <doloadama@outlook.com>
2022-10-31 06:39:39 +0000

    .

---
+++ 0x1E-search_algorithms/search_algos.c
@@ -0,0 +1,24 @@
+#ifndef _SEARCH_ALGOS_
+#define _SEARCH_ALGOS_
+
+#include <stddef>
+
+int linear_search(int *array, size_t size, int value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* _SEARCH_ALGOS */
