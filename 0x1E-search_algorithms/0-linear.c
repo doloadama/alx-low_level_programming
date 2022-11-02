c96fbca8c865cf1af5e55b9ccadd2abd04b76437
doloadama <doloadama@outlook.com>
2022-11-02 21:32:36 +0000

    .

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,25 @@
 #include "search_algos.h"
 
 /**
- * linear_search - function that searches for a value in an array
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return:  the first index where value is located 
- */
+* linear_search - searches for a value in a sorted array of integers
+* @array: array of integers
+* @size: size of array
+* @value: value to search for
+* Return: index of value or -1 if not found
+*/
+
 
 int linear_search(int *array, size_t size, int value)
 {
+size_t inc;
 
 if (array == NULL)
-return (-1)
-
-
-for (int i = 0; i < (int)size; i++)
+return (-1);
+for (inc = 0; inc < size; inc++)
 {
-printf("value checked array[%d] = [%d]\n", i, array[i]);
-if (array[i] == value)
-return (i);
+printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+if (array[inc] == value)
+return (inc);
 }
 return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,12 @@
 #ifndef _SEARCH_ALGOS_
 #define _SEARCH_ALGOS_
 
-#include <stddef>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 
 
