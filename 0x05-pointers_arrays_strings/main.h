a1de7ddb0f13d02ea64f261f9030a4f47b01f94d
doloadama <doloadama@outlook.com>
2023-01-13 09:51:53 +0000

    100-atoic

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,7 +9,8 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
+int _atoi(char *s);
+  void print_array(int *a, int n);
 
 
 
