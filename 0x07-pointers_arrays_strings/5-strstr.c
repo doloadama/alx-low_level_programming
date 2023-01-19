6039473558d536e84f8f8e4da5a01a49488c1a34
doloadama <doloadama@outlook.com>
2023-01-19 18:24:19 +0000

    .

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,34 @@
 #include "main.h"
-#define NULL 0
 /**
- * _strstr - locates a substring
- * @haystack: the string
- * @needle: target substring to search
- * Return: haystack
- */
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, j, k;
+int i;
 
-if (needle[0] == '\0')
+if (*needle == 0)
 return (haystack);
-while (haystack[i] != '\0')
-{
-if (haystack[k] == needle[0])
 
-x = i, j = 0;
-i++, j++;
-break;
-}
-if (needle[i] == needle[j]
+while (*haystack)
 {
-return (haystack + i);
-}
-}
+i = 0;
+
+if (haystack[i] == needle[i])
+{
+do {
+if (needle[i + 1] == '\0')
+return (haystack);
 i++;
+} while (haystack[i] == needle[i]);
 }
-return (NULL);
+haystack++;
+}
+return ('\0');
 }
---
+++ 0x07-pointers_arrays_strings/5-strstr.c~
@@ -0,0 +1,32 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: target substring to search
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, j, k;
+
+if (needle[0] == '\0')
+return (haystack);
+while (haystack[i] != '\0')
+{
+if (haystack[k] == needle[0])
+
+x = i, j = 0;
+i++, j++;
+break;
+}
+if (needle[i] == needle[j]
+{
+return (haystack + i);
+}
+}
+i++;
+}
+return (NULL);
+}
